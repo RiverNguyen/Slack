b78bc5a0e59e7aa8721b3227e276485165b32782
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-03-28 20:22:18 +0700

    ADD: Integrate workspaces functionality with API queries and dialog components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useEffect, useMemo } from "react";
 
 const Home = () => {
+  const { data, isLoading } = useGetWorkspaces();
+  const workspacesId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspacesId) {
+      console.log("Redirecting to workspace");
+    } else {
+      console.log("Opening workspace creation modal");
+    }
+  }, [workspacesId, isLoading]);
+
   return (
     <div>
       <UserButton />
