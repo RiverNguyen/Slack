551eea092ebda4e984d284dec7ed790e4d0c0350
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-03-29 16:50:52 +0700

    ADD: Implement workspace creation functionality with modal and API integration

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,29 @@
-import { query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, { name }) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new ConvexError("User not authenticated");
+    }
+
+    const joinCode = "123456";
+
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name,
+      userId,
+      joinCode,
+    });
+
+    return workspaceId;
+  },
+});
 
 export const get = query({
   args: {},
--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -9,12 +9,30 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useState } from "react";
 
 export const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
+
+  const { mutate } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name },
+      {
+        onSuccess(data) {
+          console.log(data);
+        },
+      }
+    );
   };
 
   return (
@@ -24,9 +42,10 @@ export const CreateWorkspaceModal = () => {
           <DialogHeader>
             <DialogTitle>Add a new workspace</DialogTitle>
           </DialogHeader>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit}>
             <Input
-              value={""}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               disabled={false}
               required
               autoFocus
