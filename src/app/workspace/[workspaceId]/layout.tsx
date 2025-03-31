4e8258bbdca664ab58fef154ea4139a39009bb5e
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-03-31 21:34:15 +0700

    ADD: Integrate resizable panels for improved workspace layout and add WorkspaceSidebar component

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 const WorkspaceIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,7 +15,20 @@ const WorkspaceIdLayout = ({ children }: { children: React.ReactNode }) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId={"jc-workspace-layout"}
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5e2c5f]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle></ResizableHandle>
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
