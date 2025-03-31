4e77cdfcb130e5ccdbf2dfdf5b0f876ce66c9fd5
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-03-31 20:37:30 +0700

    ADD: Implement members table and update workspace creation and retrieval logic

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -21,6 +21,12 @@ export const create = mutation({
       joinCode,
     });
 
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
     return workspaceId;
   },
 });
@@ -28,16 +34,39 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
 export const getById = query({
   args: { id: v.id("workspaces") },
   handler: async (ctx, { id }) => {
-    const useId = await getAuthUserId(ctx);
+    const userId = await getAuthUserId(ctx);
 
-    if (!useId) {
+    if (!userId) {
       throw new ConvexError("User not authenticated");
     }
 
@@ -45,6 +74,19 @@ export const getById = query({
 
     if (!workspace) {
       throw new ConvexError("Workspace not found");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", id).eq("userId", userId)
+      )
+      .unique();
+
+    console.log("member", member);
+
+    if (!member) {
+      return null;
     }
 
     return workspace;
