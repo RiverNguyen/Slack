887a86ecf9261416fe1f0102dec2f5a6821b66c5
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-04-15 17:42:13 +0700

    feat: add SidebarItem component for improved workspace navigation

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,7 +1,13 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader } from "lucide-react";
+import {
+  AlertTriangle,
+  Loader,
+  MessageSquareText,
+  SendHorizonal,
+} from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
 import { WorkspaceHeader } from "./workspace-header";
 
 export const WorkspaceSidebar = () => {
@@ -37,6 +43,15 @@ export const WorkspaceSidebar = () => {
         workspace={workspace}
         isAdmin={member.role === "admin"}
       />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem
+          label="Threads"
+          icon={MessageSquareText}
+          id="threads"
+          variant={"active"}
+        />
+        <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
+      </div>
     </div>
   );
 };
